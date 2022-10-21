90924a49f7808c8dba3e88d5016cb9a7d889d141
Nelson <109478547+buildsites@users.noreply.github.com>
2022-10-21 06:09:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var name = prompt ("Hello Gamer, input Your Name to Enter Stage One of this number guessing game");
 var n = Math.random();
-n = n * 3;
+n = n * 2;
 n = Math.floor(n) + 1;
 console.log(n);
-var guess = prompt (  name +" Welcome"+" to stage one guess the number in my Console to go to stage two")
+var guess = prompt (  name +" Welcome"+" to stage one. Guess a number between 1 and 2")
 
 if (guess == n){
-    alert("correct");
+    alert("correct. you have earned 10 points");
     var p = Math.random();
-     p = p * 4;
+     p = p * 3;
 p = Math.floor(p) + 1;
 console.log(p);
-var guess2 = prompt (  name +" Welcome"+" to stage to Guess the Number in my console to finish game");
+var guess2 = prompt (  name +" Welcome"+" to stage two Guess the Number in my console to finish game. hint: The number is ranging from 1 to 3");
  if (guess2 == p){
-    alert("Congrats " + name + "You finished the game")
- } else {alert("think you need a reboost, Try again")} 
+    alert("Congrats " + name + " You finished the game, and received 20 points")
+ } else {alert("I think you need a reboost, Try again")} 
 
 } else {alert("I thought You were a genius lol, try again")}
